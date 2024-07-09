e68f9edfe780af51a435f03865a5ab458f5fc139
Chekuri Divya <divyachekuri00@gmail.com>
2024-07-09 18:51:07 +0530

    Add files via upload

---
+++ forloop.js
@@ -0,0 +1,5 @@
+let eids = [101, 102, 103, 104]
+for (let i = 0; i <= 3; i++) {
+    console.log(eids[i])
+}
+
---
+++ three.js
@@ -0,0 +1,4 @@
+let enames = ["Rahul", "Sonia", "Priyanka", "Modi", "Amith"]
+for (ename of enames) {
+    console.log(ename)
+}
